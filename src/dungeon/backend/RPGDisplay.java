914914d96a0663eea2e086019a6dd4179ae45995
Gregor Peach <gregorpeach@gmail.com>
2014-11-08 13:42:08 -0800

    Added simple setup

--- src/dungeon/backend/RPGDisplay.java
+++ src/dungeon/backend/RPGDisplay.java
@@ -1,4 +1,10 @@
 package dungeon.backend;
 
+import dungeon.frontend.DungeonManager;
+
 public class RPGDisplay {
+
+	public RPGDisplay(DungeonManager mainManager) {
+		// TODO Auto-generated constructor stub
+	}
 }
--- src/dungeon/main/Main.java
+++ src/dungeon/main/Main.java
@@ -1,9 +1,13 @@
 package dungeon.main;
 
+import dungeon.backend.RPGDisplay;
+import dungeon.frontend.DungeonManager;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		DungeonManager mainManager=new DungeonManager();
+		new RPGDisplay(mainManager);
 	}
 
 }
