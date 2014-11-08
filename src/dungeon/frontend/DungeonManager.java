1c195ce590e740d262e3641d2f2a3bac2d3ece1b
Gregor Peach <gregorpeach@gmail.com>
2014-11-08 14:00:33 -0800

    Started updating to fufill interface

--- src/dungeon/frontend/DungeonManager.java
+++ src/dungeon/frontend/DungeonManager.java
@@ -1,7 +1,30 @@
 package dungeon.frontend;
 
+import dungeon.connectionInterfaces.DrawableCell;
 import dungeon.connectionInterfaces.DungeonManagerInterface;
 
 public class DungeonManager implements DungeonManagerInterface{
+
+	public DungeonManager(){
+		
+	}
+
+	@Override
+	public DrawableCell getCellAt(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMapWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMapHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
